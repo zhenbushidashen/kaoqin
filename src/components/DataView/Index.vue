976fd158709a9f6389613609822d486fb8fd0bcb
zhangzhenx <zheniscool@163.com>
2018-05-09 23:06:36 +0800

    地图

--- src/components/DataView/Index.vue
+++ src/components/DataView/Index.vue
@@ -17,6 +17,39 @@
   <el-button type="primary">查询</el-button>
   <el-button type="primary">导出EXCEL</el-button>
   </div>
+  <div>
+    <el-table
+    :data="tableData"
+    style="width: 100%">
+
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="打卡时间"
+      :formatter="formatter">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="打卡类型"
+      :formatter="formatter">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="打卡结果"
+      :formatter="formatter">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="备注"
+      :formatter="formatter">
+    </el-table-column>
+  </el-table>
+  </div>
   
   </div>
 </template>
@@ -25,7 +58,40 @@
 export default {
    data() {
       return {
-        value4: [new Date(2016, 9, 10, 8, 40), new Date(2016, 9, 10, 9, 40)]
+        value4: [new Date(2016, 9, 10, 8, 40), new Date(2016, 9, 10, 9, 40)],
+         tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+          tag: '家'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄',
+          tag: '公司'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄',
+          tag: '家'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄',
+          tag: '公司'
+        }]
+      }
+    },
+     methods: {
+      formatter(row, column) {
+        return row.address;
+      },
+      filterTag(value, row) {
+        return row.tag === value;
+      },
+      filterHandler(value, row, column) {
+        const property = column['property'];
+        return row[property] === value;
       }
     }
 }
--- src/components/Map/Index.vue
+++ src/components/Map/Index.vue
@@ -9,30 +9,37 @@ export default {
   name: 'map',
   mounted() {
     var map = new BMap.Map(document.querySelector('.map_container'))
-    var point = new BMap.Point(116.404, 39.915)
-    map.centerAndZoom(point, 15)
-    var geolocation = new BMap.Geolocation();
-    geolocation.getCurrentPosition(function(r){
+    map.enableScrollWheelZoom(true)
+    
+ //get position
+  map.addEventListener("dragend", function(){    
+    var center = map.getCenter();    
+})
+map.addEventListener("click", function(e){    
+    map.panTo(e.point);   
+    var marker = new BMap.Marker(e.point);        // 创建标注    
+map.addOverlay(marker); 
+});
+ var geolocation = new BMap.Geolocation();
+
+  geolocation.getCurrentPosition(function(r){
+    console.log(r)
 	if(this.getStatus() == BMAP_STATUS_SUCCESS){
 		var mk = new BMap.Marker(r.point);
 		map.addOverlay(mk);
 		map.panTo(r.point);
-		alert('您的位置：'+r.point.lng+','+r.point.lat);
-	}
-	else {
-		alert('failed'+this.getStatus());
+    map.centerAndZoom(r.point, 15)
+	} else {
+		alert('定位失败'+this.getStatus());
 	}        
-});
-    window.setTimeout(function(){  
-    map.panTo(new BMap.Point(116.409, 39.918));    
-}, 2000)
+})
   }
 }
 </script>
 
 <style scoped>
   .map_container {
-      width: 650px;
-      height: 300px;
+      width: 100%;
+      height: 400px;
   }
 </style>
